8a769c4c22d4b7a721e12704221d768ee6e31080
Henok Niguse <111225297+henok26@users.noreply.github.com>
2022-12-21 19:49:57 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+struct node
+{
+int value;
+int info;
+};
+void display(node );
+node getdata(node);
+
+
+int main()
+{
+    node n,temp;
+   temp= getdata(n);
+
+
+
+    display(temp);
+
+    return 0;
+}
+void display(node n)
+{
+     cout << n.info << endl;
+     cout << n.value << endl;
+}
+node getdata(node n){
+  cout << "enter value" << endl;
+   cin>>n.value;
+    cout << "enter info" << endl;
+    cin>>n.info;
+    return n;}
